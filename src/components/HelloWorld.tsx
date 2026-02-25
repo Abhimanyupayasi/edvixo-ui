2e53ad5d7edd99c0bba4ab1917b66d44b0c1dc2b
Abhimanyu <abhimanyupayasi01@gmail.com>
2026-02-25 13:57:41 +0530

    added

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -6,7 +6,7 @@ export default function HelloWorld() {
         
         <span className="relative flex size-3">
   <span className="absolute inline-flex h-full right-0 w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
-  <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+  <span className="relative inline-flex size-3 rounded-full bg-sky-600"></span>
   
 </span>
 <h1 className="text-nowrap text-4xl">Hello World</h1>
