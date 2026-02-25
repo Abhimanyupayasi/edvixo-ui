53c0ba6b9573fd527af03f10db3dd9d50f31eec4
Abhimanyu <abhimanyupayasi01@gmail.com>
2026-02-25 13:11:44 +0530

    added

--- src/components/TestCompo.tsx
+++ src/components/TestCompo.tsx
@@ -3,6 +3,7 @@
 export default function TestCompo({ data }: any) {
   return (
     <section className="py-24 bg-blue-100 flex justify-center">
+      
       <h2 className="text-4xl text-red-800 font-bold">Steps</h2>
     </section>
   );
